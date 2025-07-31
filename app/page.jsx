849083d59e5daf21f9d69e76aae7f3201407b951
Yuri Ferreira Cavallini <76496326+cvlln1@users.noreply.github.com>
2025-07-31 20:49:54 +0000

    cleanup: remove linha desnecessária

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,6 @@ const Home = () => {
 
             {/* Apresentação */}
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Um estudante de Ciência da Computação.</span>
             <h1 className="h1 mb-6">Olá, eu sou<br /><span className="text-blue-500">Yuri Cavallini</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
